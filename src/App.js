8fa481cf7a17b2d3c9b022cc523ad6d8dffdc8eb
Silvano Burato <silvanoburato@gmail.com>
2020-12-10 09:54:22 -0300

    Criacão da estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Titulo from "./components/Titulo"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="temas-app">
+      <Titulo title = "Sign up to my newsletter" />
+      <form className="temas-form" onSubmit={() => {}}> 
+        <input 
+          type="text"
+          placeholder="my@email.com"
+          className="temas-input"
+        />      
+        <button className="temas-button">Sign up</button>        
+      </form>
     </div>
   );
 }
---
+++ src/components/Titulo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Titulo = (props) => {
+  return(
+    <header id="titulo"><h1>{ props.title }</h1></header>
+  );
+}
+
+export default Titulo;
